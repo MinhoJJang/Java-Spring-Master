c379e4d8ec72f9657c868c08fdf0d4295e861504
MinhoJJang <ahchjang@naver.com>
2023-02-05 23:33:14 +0900

    0205

--- Chapter2/src/ch09/myown/Student.java
+++ Chapter2/src/ch09/myown/Student.java
@@ -3,9 +3,9 @@ package ch09.myown;
 public class Student {
     private String studentName;
     private String studentID;
-    private Subject[] studentSubject;
+    private 
 
-    public Student(String studentName, String studentID, new Subject[] studentSubject){
+    public Student(String studentName, String studentID, Subject[] studentSubject){
         this.studentName = studentName;
         this.studentID = studentID;
         this.studentSubject = studentSubject;
